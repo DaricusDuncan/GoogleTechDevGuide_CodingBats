e5d10adc411be8c06164a0adf91a6ca807776be5
DaricusDuncan <ddd155@msstate.edu>
2017-11-29 22:03:41 -0600

    Finished maxSpan.java exercise
    This is working code.

--- maxSpan.java
+++ maxSpan.java
@@ -1,12 +1,24 @@
 public int maxSpan(int[] nums) {
   
-  int leftMost = 0;
   
-  for(int i = 0; i < nums.length; i++){
-    leftMost = nums[0];
-    
-    
+  int span = 0;
+  int greatestSpan = 0;
+  
+  
+  if(nums.length == 0){ return 0; }
+  if(nums.length == 1){ return 1; }
+  else{
+    for(int left = 0; left < nums.length; left++){
+      for(int right = 1; right < nums.length; right++){
+        if(nums[left] == nums[right]){
+          span = (right - left) + 1;
+          if(span > greatestSpan){
+            greatestSpan = span;
+          }
+        }
+      }
+    }
   }
  
- return leftMost; 
+ return greatestSpan; 
 }
