a0d0028af345974d08531830f6d3cc2629411231
DaricusDuncan <ddd155@msstate.edu>
2017-12-05 00:14:35 -0600

    Upload draft for canBalance.java
    http://codingbat.com/prob/p158767

---
+++ canBalance.java
@@ -0,0 +1,10 @@
+public boolean canBalance(int[] nums) {
+  int left = 0;
+  int right = 0;
+  if(nums.length == 0 || nums.length == 1){ return false; }
+  
+  for(int i = 0; i < nums.length; i++){
+    
+  }
+  return true;
+}
