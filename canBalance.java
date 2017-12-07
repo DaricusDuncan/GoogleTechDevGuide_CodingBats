58c2233d79790a4089e21beaa664da680efa514a
DaricusDuncan <ddd155@msstate.edu>
2017-12-06 22:26:30 -0600

    Completed canBalance exercise
    Finished assignment

--- canBalance.java
+++ canBalance.java
@@ -1,10 +1,16 @@
 public boolean canBalance(int[] nums) {
-  int left = 0;
-  int right = 0;
+  int[] leftArray, rightArray;
+  int left = 0, right = 0;
   if(nums.length == 0 || nums.length == 1){ return false; }
   
   for(int i = 0; i < nums.length; i++){
+    leftArray = Arrays.copyOfRange(nums, 0, i);
+    rightArray = Arrays.copyOfRange(nums, i, nums.length);
     
+    left = Arrays.stream(leftArray).sum();
+    right = Arrays.stream(rightArray).sum();
+    
+    if(left == right){ return true; }
   }
-  return true;
+  return false;
 }
